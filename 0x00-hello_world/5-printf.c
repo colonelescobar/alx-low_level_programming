ce109d5b4952af85c853291f3498d6d56dc04877
colonelescobar <tiagati007@gmail.com>
2022-04-09 09:36:22 -0700

    first commit for task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ * Filename: 5-printf.c
+ * Author: Yinti Agati <tiagati@yahoo.com>
+ *
+ * Returns 0 always.
+ */
+
+#include <stdio.h>
+
+/* main - This program prints the phrase "with proper grammar, but the outcome is a piece of art,"
+ *
+ * returns 0 always.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return 0;	
+}
