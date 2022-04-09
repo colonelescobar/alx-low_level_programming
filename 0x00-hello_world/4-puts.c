2a3fee2dff1759199ff17286586047e80588f5e4
colonelescobar <tiagati007@gmail.com>
2022-04-09 09:13:14 -0700

    another emndment for  task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,16 @@
  * File: 4-puts
  * Author:yinti Agati <tiagati@yahoo.com>
  */
+
 #include <stdio.h>
 
 /* 
- * This program is to print the statement "Programming is like building a multilingual puzzle"
+ * main - This program is to print the statement "Programming is like building a multilingual puzzle"
  * followed by a newline.
  * It always returns 0.	
  */
 int main(void)
 {
-
 	puts("Programming is like building a multilingual puzzle\n");
 	
 	return (0);
