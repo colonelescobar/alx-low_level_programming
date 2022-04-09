d3c7cbcacf48949e630798edfa9719e42a91608b
colonelescobar <tiagati007@gmail.com>
2022-04-09 09:19:20 -0700

    amendment no 4 for task 4-puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,19 @@
 /*
- * File: 4-puts
- * Author:yinti Agati <tiagati@yahoo.com>
+ * File: 4-puts.c
+ * Auth: Yinti Agati <tiagati@yahoo.com>
  */
 
 #include <stdio.h>
 
-/* 
- * main - This program is to print the statement "Programming is like building a multilingual puzzle"
- * followed by a newline.
- * It always returns 0.	
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	
+
 	return (0);
 }
