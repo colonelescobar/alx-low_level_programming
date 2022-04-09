253911c200114c247fba6f47051db47056b6c7ec
colonelescobar <tiagati007@gmail.com>
2022-04-09 08:58:55 -0700

    first commit for task 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/* 
+ * task 4-Puts.c 
+ *This program is to print the statemendt "Programming is like building a multilingual puzzle"
+ */
+int main(void)
+{
+
+	puts("Programming is like building a multilingual puzzle\n");
+	
+	return (0);
+}
+
