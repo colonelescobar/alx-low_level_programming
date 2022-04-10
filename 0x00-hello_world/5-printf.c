ae2c3455899cad75538945de1dfbcbbe31bcbd08
colonelescobar <tiagati007@gmail.com>
2022-04-10 12:20:16 -0700

    task 6 commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,14 +7,15 @@
 
 #include <stdio.h>
 
-/* main - This program prints the phrase "with proper grammar, but the outcome is a piece of art,"
+/**
+ * main - This program prints the phrase "with proper grammar, but the outcome is a piece of art,"
  *
  * returns 0 always.
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return 0;	
 }
