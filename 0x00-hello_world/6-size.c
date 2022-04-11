4a3e70e8a1582463d8d9eb8e13f20247a4aa4916
colonelescobar <tiagati007@gmail.com>
2022-04-11 08:29:06 -0700

    comment correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
 #include<stdio.h>
 
 /**
- * main:A C program that prints the size of various types on the computer
+ * main - A C program that prints the size of various types on the computer
  *		it is compiled and run on.
  *
  *
