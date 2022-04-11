5a1fb714a77f0e635557372ac6fbd529116f4b57
colonelescobar <tiagati007@gmail.com>
2022-04-11 08:38:58 -0700

    another commit for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@
 
 int main(void)
 {
-	printf("Size of char: %d", sizeof(char));
-	printf("Size of char: %d", sizeof(int));
-	printf("Size of char: %d", sizeof(long int));
-	printf("Size of char: %d", sizeof(long long int));
-	printf("Size of char: %d", sizeof(float));
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of a long int: %zu bytes \n", sizeof(long int));
+	printf("Size of long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of float: %zu bytes\n", sizeof(float));
 
 	return (0);
 
