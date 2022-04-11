846dfc7e0fe1a2be6642ba69f7c923cadfc2052e
colonelescobar <tiagati007@gmail.com>
2022-04-11 08:27:02 -0700

    first commit for task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+/*
+ * File 6-size.c
+ *
+ * Author: yinti agati <tiagati@yahoo.com>
+ */
+
+#include<stdio.h>
+
+/**
+ * main:A C program that prints the size of various types on the computer
+ *		it is compiled and run on.
+ *
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	printf("Size of char: %d", sizeof(char));
+	printf("Size of char: %d", sizeof(int));
+	printf("Size of char: %d", sizeof(long int));
+	printf("Size of char: %d", sizeof(long long int));
+	printf("Size of char: %d", sizeof(float));
+
+	return (0);
+
+}
