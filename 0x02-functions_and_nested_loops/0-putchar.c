cb7cb78c161759c705eef5ccf7495b859ba6667a
Yinti Agati <yinti.agati@phed.com.ng>
2022-04-13 00:00:57 +0100

    first commmit for this task

---
+++ 0x02-functions_and_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/*
+ * main - This code prints "_putchar" and adds a newline.
+ *
+ * Return: putchar.
+ *
+ */
+
+char main(void)
+{
+	char result;
+	result = "_putchar\n";
+
+	return (result);
+
+
+}
