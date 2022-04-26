affbf46e064b68c6ccd9262a66d5b59a6ed3c7fe
colonelescobar <tiagati@yahoo.com>
2022-04-26 14:19:16 +0100

    betty conformance

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - This code prints the alphabets 10 times.
+ * print_alphabet_x10 - This code prints the letters of the alphabets 10 times.
  *
  * return : always returns 0
  */
@@ -10,14 +10,13 @@ void print_alphabet_x10(void)
 {
 	int count = 0;
 	char c;
-	
+
 	while (count < 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-		
 		count++;
 		_putchar('\n');
 	}
