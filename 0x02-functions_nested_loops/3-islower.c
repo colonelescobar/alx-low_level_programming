1557f9ce6415d96dc44ce9d6a068320a43bedc10
colonelescobar <tiagati@yahoo.com>
2022-04-26 16:02:04 +0100

    edited documentation for task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,15 +6,14 @@
  *
  */
 
-
 int _islower(int c)
 {
-	if ( c >= 'a' && c <= 'z' )
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	}
+	else
+	{
+		return (0);
+	}
 }
