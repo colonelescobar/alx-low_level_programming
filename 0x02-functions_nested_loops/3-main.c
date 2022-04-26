a8475d8897436a71e540638f5b6eb1fb1ef97077
colonelescobar <tiagati@yahoo.com>
2022-04-26 15:10:51 +0100

    task 2 commit

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - This function returns a value of 1 if the case of an alphabet
+ * is lower and 0 if it isn;t.
+ *
+ */
+
+
+int _islower(int c)
+{
+	int c;
+
+	if ( c >= 'a' && c <= 'z' )
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
