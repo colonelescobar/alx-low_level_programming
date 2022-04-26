20b02cc7b6ed60fb7d1586d86ff3cd264a37494a
colonelescobar <tiagati@yahoo.com>
2022-04-26 22:58:02 +0100

    comment edit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - This function returns a value of 1 if the case of an alphabet is
- * 		lower and 0 if it isn't.
+ * _islower - returns 1 if lowercase, else 0.
  *
  * @c: takes in a character.
  *
