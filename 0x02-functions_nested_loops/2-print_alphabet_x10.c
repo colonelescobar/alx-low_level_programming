929e4d50b538162be8298b05de3fc9f910ff16b6
colonelescobar <tiagati@yahoo.com>
2022-04-26 10:57:50 +0100

    created a function that prints the alphabets in lower case

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - This code prints the alphabets 10 times.
+ *
+ * return : always returns 0
+ */
+
+int main(void)
+{
+	int count;
+	
+	for (count = 0; count <=10; count++)
+	{
+		_putchar("")
+	}
+}
