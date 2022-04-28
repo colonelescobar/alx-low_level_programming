b1578e6e28bb9b1c98e9d76804d8e6bef2ab0319
colonelescobar <tiagati@yahoo.com>
2022-04-28 14:02:02 +0100

    11-print_to_98.c commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,28 +6,29 @@
  *		input till the integer 98 while printing every decrement/increment.
  *		prints a newline when done.
  * @n: input integer.
- * 
+ *
  * Return: int.
  */
 
 void print_to_98(int n)
 {
-		
-	if ( n >= 98)
+
+	if (n >= 98)
 	{
-		while (n !=98)
+		while (n != 98)
 		{
-			printf("%d",n);
-			n--;
+			printf("%d, ", n--);
 		}
-		printf("%d\n", n);
+		printf("%d,", n);
 	}
 
-	else if (n <= 98)
+	else if (n < 98)
 	{
-		while (n < 98)
+		while (n != 98)
 		{
-			printf("%d", n++);
+			printf("%d, ", n++);
 		}
+		printf("%d.", n);
 	}
+	printf("\n");
 }
