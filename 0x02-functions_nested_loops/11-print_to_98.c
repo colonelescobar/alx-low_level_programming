7d9c4a31f2defd7b7e13c4a1b57d505fa069fc54
colonelescobar <tiagati@yahoo.com>
2022-04-28 14:06:50 +0100

    11-print_to_98.c commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n--);
 		}
-		printf("%d.", n);
+		printf("%d", n);
 	}
 
 	else if (n < 98)
@@ -28,7 +28,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n++);
 		}
-		printf("%d.", n);
+		printf("%d", n);
 	}
 	printf("\n");
 }
