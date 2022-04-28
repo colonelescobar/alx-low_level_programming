0670bdd680fd638bd303c6e285fb906665e5ddd1
colonelescobar <tiagati@yahoo.com>
2022-04-28 16:27:50 +0100

    task 12 commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,9 +3,9 @@
 
 /**
  * print_times_table - A funciton that prints a times table when n is
- * less than 15 and greater than 0.
+ *			less than 15 and greater than 0.
  *
- * @n - number input for the time table to be printed.
+ * @n - 'n' isnumber input for the time table to be printed.
  *
  */
 
