9402129071d35b0cf5bb3eb70a56490b44f8ab22
colonelescobar <tiagati@yahoo.com>
2022-04-28 15:14:12 +0100

    task 12 commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - A funciton that prints a times table when n is
+ * less than 15 and greater than 0.
+ *
+ * Return: returns nothing.
+ *
+ */
+
+void print_times_table(int n)
+{
+	int i,j;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j < n; j++) 
+			{
+				printf("%d,	", i*j);
+			}
+			printf("%d", i*n);
+			printf("\n");
+		}
+	}
+	printf("\n");
+}
