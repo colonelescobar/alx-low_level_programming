8dc8f68bbd7ce2a80e15682e01af307ce5182ba3
colonelescobar <tiagati@yahoo.com>
2022-05-01 14:30:30 +0100

    task 100 commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,15 +16,16 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
+			printf("0");
 
-			for (j = 0; j < n; j++)
+			for (j = 1; j < n; j++)
 			{
 
-				printf("%d,   ", i * j);
+				printf("%*d,", 4, i * j);
 
 			}
 
-			printf("%d\n", i * j);
+			printf("%*d\n", 4, i * j);
 		}
 		printf("\n");
 	}
