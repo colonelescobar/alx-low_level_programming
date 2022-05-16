ee2f0c8e9194d27cb8b4b4b73c65b92bf339f0f2
colonelescobar <tiagati@yahoo.com>
2022-05-16 16:37:29 +0100

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - This function multiplies to digits.
+ * @a: one of the inputs.
+ * @b: Second input
+ * @Product: Variable that stores result of a*b.
+ *
+ * return: returns result.
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return(result);
+}
