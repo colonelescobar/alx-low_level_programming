55535db55f4becb98a3995cb21dcd891dcb76be0
colonelescobar <tiagati@yahoo.com>
2022-05-16 16:30:59 +0100

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,16 +3,15 @@
 
 /**
  * _isdigit - Function checks if input is digit or not.
+ *@c: input character
  *
- * @c: input character
- *
- *Retur: returns 1 if @c is a digit and 0 otherwise.
+ *Return: returns 1 if @c is a digit and 0 otherwise.
  */
 
 int _isdigit(int c)
 {
 
-	if ( c >= 48 && c <= 57)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
