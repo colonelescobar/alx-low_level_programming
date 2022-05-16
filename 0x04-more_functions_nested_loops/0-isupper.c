cfc8d4ea6f4ea3f8c5fc75be79493fe37253d9ad
colonelescobar <tiagati@yahoo.com>
2022-05-16 15:53:25 +0100

    task 0 commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper: Checks the case of an input, whether uppercase/lowercase.
+ *
+ *
+ * return: returns 1 when uppercase and/or 0 otherwise.
+ *
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 98)
+	{
+		return (1);
+	}
+
+	return (0);
+}
