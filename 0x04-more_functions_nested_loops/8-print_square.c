8f0386aa6bb03b6f0aa5d790c6bea29b2e1d7abe
colonelescobar <tiagati@yahoo.com>
2022-05-17 16:29:55 +0100

    task 9

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints a square using # symbol.
+ * @size: input of square size.
+ *
+ * Return: returns nothing
+ */
+
+void print_square(int size)
+{
+	int i,j;
+
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
+
