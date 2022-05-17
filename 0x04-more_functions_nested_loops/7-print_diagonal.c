52a5a9acafdefe50b5edae07e0919a381a71248d
colonelescobar <tiagati@yahoo.com>
2022-05-17 13:14:44 +0100

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - prints '\' n times diagonally.
+ * @n: number of itmes the character '\' should be printed.
+ *
+ * Return: nothing.
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j = 0;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n ; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
