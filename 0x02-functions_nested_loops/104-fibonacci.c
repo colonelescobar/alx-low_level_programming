b43b86fc225bc82d998a26aa696c38da767de0ae
colonelescobar <tiagati@yahoo.com>
2022-05-21 18:12:28 +0100

    task 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,23 +12,36 @@
 
 int main(void)
 {
-	unsigned long long int counter;
-	unsigned long long int a = 1, b = 2;
-	unsigned long long int c;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	printf("%llu, %llu, ", a, b);
-	for (counter = 1; counter <= 96; counter++)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		if (counter != 48)
-		{	c = a + b;
-			printf("%llu, ", c);
-			a = b;
-			b = c;
-		}
-
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
-	c = a + b;
-	printf("%llu\n", c);
-	return (0);
 
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
+
+	return (0);
 }
