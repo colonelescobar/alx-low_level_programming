d50f32c216c85badc62c90124a7478fdfe486672
colonelescobar <tiagati@yahoo.com>
2022-05-21 17:05:49 +0100

    task 101

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,31 +2,33 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 50 elements of the fibonnacci series starting from 1.
+ * main - prints the first 50 elements of the fibonnacci series starting
+ * from 1.
  *
  *
- * return: return 0 when done.
+ * Return: return 0 when done.
  */
 
 
 int main(void)
 {
-	long unsigned counter;
-	long unsigned a = 1, b = 2;
-	long unsigned c;
+	unsigned long counter;
+	unsigned long a = 1, b = 2;
+	unsigned long c;
 
 	printf("%lu, %lu, ", a, b);
-	for (counter = 1; counter <= 49; counter++)
+	for (counter = 1; counter <= 48; counter++)
 	{
-		if( counter != 48)
+		if (counter != 48)
 		{	c = a + b;
-			printf("%lu, ", c );
+			printf("%lu, ", c);
 			a = b;
 			b = c;
 		}
-		
+
 	}
+	c = a + b;
 	printf("%lu\n", c);
 	return (0);
-	
+
 }
