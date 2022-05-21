eb9261122347489747ba706f20d592c11b56a8de
colonelescobar <tiagati@yahoo.com>
2022-05-21 15:16:13 +0100

    task 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,7 +4,7 @@
 
 /**
  * main - this program prints a series of natural numbers that are factors of
- * 		1024.
+ *		1024.
  *
  * Return: Nothing.
  */
@@ -13,14 +13,13 @@ int main(void)
 {
 	int n;
 	int sum = 0;
-
-	for (n = 0;n <= 1024; n++)
-		 {
-			if (n % 3 == 0 || n % 5 == 0)
-			{
-				sum += n;
-			}
-		 }
+	for (n = 0; n <= 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum += n;
+		}
+	}
 	printf("%d\n", sum);
 	return (0);
 }
