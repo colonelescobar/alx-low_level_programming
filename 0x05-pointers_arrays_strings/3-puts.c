6b0a8eead0d24494c0922fcf0a84452a7fd96811
colonelescobar <tiagati@yahoo.com>
2022-05-28 00:15:02 +0100

    task 2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,12 +7,12 @@
  *Return: void that means our answer is correct
  */
 
-int _strlen(char *s)
+int _strlen(char *str)
 {
 	int u;
 
 	u = 0;
-	while (s[u] != '\0')
+	while (str[u] != '\0')
 	{
 		u++;
 	}
