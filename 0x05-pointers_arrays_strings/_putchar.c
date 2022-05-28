c97035c02c9af4b786c2e551873b406330cfeec1
colonelescobar <tiagati@yahoo.com>
2022-05-28 12:39:41 +0100

    putchar file

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * -putchar - prints characters of the variable c unto the screen.
+ * Return: 1 on success
+ * on error, -1 is returned.
+ * 
+ */
+
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
