6a2cc24f6dab1ae45daf9f8cefff9a83784401bc
colonelescobar <tiagati@yahoo.com>
2022-05-31 17:12:10 +0100

    task 0x05w

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,21 +2,22 @@
 #include <stdio.h>
 
 /**
- *_strlen -length of a string
- *@s: A pointer to an int that will be changed/updated
+ *_puts - prints the characters of a string.
+ * @s: A pointer to an int that will be changed/updated
  *
  *Return: void that means our answer is correct
  */
 
-void _puts (char *str)
+void _puts(char *str)
 {
 	int u;
 
 	u = 0;
 	while (str[u] != '\0')
 	{
-		u++;
+		
 		_putchar(str[u]);
+		u++;
 	}
 	printf("\n");
 
