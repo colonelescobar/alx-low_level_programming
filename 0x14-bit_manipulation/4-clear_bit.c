e81df539f2760b8a979626cccdf8effa9f129261
colonelescobar <tiagati@yahoo.com>
2022-05-31 03:19:51 +0100

    omo

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
-  * set_bit - sets the value of a bit to 1 at a given index
+  * clear_bit - sets the value of a bit to 0 at a given index
   * @n: unsigned long to change
-  * @index: index to change to one
+  * @index: index to change to zero
   * Return: 1 if it worked or -1 if an error occurred
   */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
+	int clear;
+
 	if (index > 53 || !n)
 		return (-1);
-	(*n |= 1 << index);
+	clear = 1 << index;
+	*n = (*n & ~clear) | ((0 << index) & clear);
 	return (1);
 }
