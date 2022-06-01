94f51441766fff7acfdf24d9c46857803a7af6e5
colonelescobar <tiagati@yahoo.com>
2022-06-01 13:54:31 +0100

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - A funciton that prints every character of a string
+ * followed by a new line.
+ * 
+ * @str: Input string.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+		printf("%c", s[i]);
+
+	}
+	printf("\n");
+}
