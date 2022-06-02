464d2ebd36dbd310ed67e1a3b6ac02aa8d29dddf
colonelescobar <tiagati@yahoo.com>
2022-06-02 18:08:47 +0100

    9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,20 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	dest = src;
+	int len, i;
 
-	printf("\n");
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
 	return (dest);
-
 }
