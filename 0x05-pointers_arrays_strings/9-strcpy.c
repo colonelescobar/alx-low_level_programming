0e839d4e864e0140f609f1de11a9a40d62f73f4e
colonelescobar <tiagati@yahoo.com>
2022-06-02 16:59:20 +0100

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - A function that copies the string pointed to by "src", including the terminating null byte "\0".
+ * @dest: destination variable.
+ * @src: source value holder.
+ * 
+ * Return: Nothing
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return(dest);
+
+}
