6ab2f3573f905271041be854a2e88f83d2c5696c
colonelescobar <tiagati@yahoo.com>
2022-06-02 16:38:19 +0100

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,10 +17,10 @@ void print_array(int *a, int n)
 	for (i = 0; i != '\0'; i++)
 	;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d", a[i]); /* code */
-		if (i < n)
+		if (i < n - 1)
 		{
 			printf(", ");
 		}
