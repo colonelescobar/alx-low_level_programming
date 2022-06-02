a47b825454cacdf015dbe7563b54102a267bbb9f
colonelescobar <tiagati@yahoo.com>
2022-06-02 15:15:33 +0100

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * puts_half - prints half of the characters of a string.
@@ -12,20 +12,21 @@ void puts_half(char *str)
 	int string_length = 0, i;
 
 	for (i = 0; str[i] != '\0'; i++)
-			;
+		;
 
-		string_length = i;
+	string_length = i;
+
 	if (string_length % 2 != 0)
 	{
 		for (i = ((string_length - 1)) / 2; str[i] != '\0'; i++)
 		{
 			_putchar(str[i]);
 		}
-	
+
 	}
 	else
 	{
-		for (i = (string_length/2); str[i] != '\0'; i++)
+		for (i = (string_length / 2); str[i] != '\0'; i++)
 		{
 			putchar(str[i]);
 		}
