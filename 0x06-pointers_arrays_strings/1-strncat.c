17e953a0a8cd8c2c24fe4d7c8907b3b7b1ffe14a
colonelescobar <tiagati@yahoo.com>
2022-06-03 18:35:58 +0100

    0-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - This function concatenates  n elements of "src"
+ * strings to dest strings and adds "\0" if n >= src length
+ * @dest: destination of concatenated strings.
+ * @src: source string.
+ * Return: concatenated strings.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = strlen(dest);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[dest_len + i] = src[i];
+
+	dest[dest_len + i] = '\0';
+	return (dest);
+	
+}
