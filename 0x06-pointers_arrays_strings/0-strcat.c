7f5052a0bba634a93539261e746fd076132f9724
colonelescobar <tiagati@yahoo.com>
2022-06-03 17:55:09 +0100

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,15 +6,15 @@
  * @dest: destination variable.
  * @src: Source value holder.
  *
- * Return: a pointer of type char *.
+ * Return: a pointer of type (char *).
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int dest_len = strlen(dest);
     int i;
 
-     for (i = 0 ; i < n && src[i] != '\0' ; i++)
+     for (i = 0; src[i] != '\0'; i++)
 	 	dest[dest_len + i] = src[i];
 
     dest[dest_len + i] = '\0';
