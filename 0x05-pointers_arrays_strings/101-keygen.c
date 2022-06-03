43699ff49924b6e074c8410d370f37a706760397
colonelescobar <tiagati@yahoo.com>
2022-06-03 17:01:51 +0100

    0x15 files

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+
+/**
+ * main - Generates random numbers. 
+ * 
+ * @return int 
+ */
+
+int main()  
+{  
+    int i;    
+  
+    for (i = 0; i <5; i++)  
+    {  
+        printf(" %d", rand());  
+     }   
+     return (0);  
+}  
