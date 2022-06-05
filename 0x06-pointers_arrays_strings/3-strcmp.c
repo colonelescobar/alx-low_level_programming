b65f6f71703498ee613a172d9b2f2840b3825e13
colonelescobar <tiagati@yahoo.com>
2022-06-05 01:08:21 +0100

    strnccmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcmp - This function compares two strings.
+ * @s1: First string.
+ * @s2: Second string.
+ *
+ * Return: integer.
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int s1_stringLen = strlen(s1);
+	int s2_stringLen = strlen(s2);
+	int match_count = 0;
+
+	for (i = 0; i < s1_stringLen; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			match_count += 1;
+		}
+		else
+			break;
+	}
+
+	if (s1_stringLen >= match_count && s1_stringLen > s2_stringLen)
+	{
+		return (1);
+	}
+	else if (s1_stringLen == s2_stringLen && match_count == s1_stringLen)
+	{
+		return (0);
+	}
+	else if (s2_stringLen >= match_count && s2_stringLen > s1_stringLen)
+	{
+		return (-1);
+	}
+
+	return (0);
+}
