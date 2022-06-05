77ea1bfc6a9dcdf249da6585ba42d3c8e010e13c
colonelescobar <tiagati@yahoo.com>
2022-06-05 12:50:53 +0100

    strnccmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,7 +17,6 @@ int _strcmp(char *s1, char *s2)
 	int s1_stringLen = strlen(s1);
 	int s2_stringLen = strlen(s2);
 	int match_count = 0;
-	int negative;
 
 	for (i = 0; i < s1_stringLen; i++)
 	{
@@ -39,7 +38,6 @@ int _strcmp(char *s1, char *s2)
 	}
 	else if (s2_stringLen >= match_count && s2_stringLen > s1_stringLen)
 	{
-		negative = 0 - (s1_stringLen + match_count);
 		return (*s1 - *s2);
 	}
 	return (0);
