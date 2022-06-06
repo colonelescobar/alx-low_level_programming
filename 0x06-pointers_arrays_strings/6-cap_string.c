6bc5aaca1d8d632ca29806967165c9b50cab3fc4
colonelescobar <tiagati@yahoo.com>
2022-06-06 18:21:40 +0100

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,17 +15,17 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ( i == 0 && (str[i] >= 'a' && str[i] <= 'z' ))
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
 		{
 			/* code */
 		}
-		
-		if ((str[i] >= 0 && str[i] <= 32) || str[i] == 46 )
-			{
-				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-					str[i + 1] = str[i + 1] - 32;
-			}
-		
+
+		if ((str[i] >= 0 && str[i] <= 32) || str[i] == 46)
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] = str[i + 1] - 32;
+		}
+
 	}
 	return (str);
 }
