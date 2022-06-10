f8b80ad7708fb03099febe64e06014fa6fc90735
colonelescobar <tiagati@yahoo.com>
2022-06-10 10:39:46 +0100

    strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,13 +9,10 @@
 
 int _strlen_recursion(char *s)
 {
-	int i;
 
-	i = 0;
 	if (*s != '\0')
 	{
-		i += 1;
-		_strlen_recursion(s + 1);
+		return (_strlen_recursion(s + 1) + 1);
 	}
-	return (i);
+	return (0);
 }
