ba3a805e161bbaacedf00f70db58fa534a3f3547
colonelescobar <tiagati@yahoo.com>
2022-06-10 10:58:34 +0100

    facatorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - A function that finds the factorial of a given numnber. 
+ *
+ * @n: input number. 
+ * Return: integer.
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (0);	/* code */
+	}
+
+	return(n * factorial(n - 1));
+	
+}
