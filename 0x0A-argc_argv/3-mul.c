9d44fd415f53a8af912b50c4fc1bf339445b002b
colonelescobar <tiagati@yahoo.com>
2022-06-11 20:41:33 +0100

    mulc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 
 /**
- *  
- * 
+ * main - This program multiplies two numbers and prints the result.
+ * @argc: arguement count.
+ * @argv: arguement content/strings.
+ * Return: int.
  */
+
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
