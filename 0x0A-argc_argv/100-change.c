17ea3d63a4463eeb9d032b0b041d7ad546770573
colonelescobar <tiagati@yahoo.com>
2022-06-11 21:28:48 +0100

    mulc and add

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,23 +1,55 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - A function that  
- * 
+ * returnCents - calculates the min number of coins needed to make change given
+ * @n: target change value
+ * Return: min num of coins
  */
-
-int main(int argc, char *argv[])
+int returnCents(int n)
 {
-	int i, j, res = 0;
+	int coins = 0;
 
-	if (argc > 1)
-		for (i = 1; i < argc; i++)
-		{
-			for (j = 0; argv[i][j]; j++)
-				if (argv[i][j] < '0' || argv[i][j] > '9')
-					return (printf("Error\n"), 1);
-			res += atoi(argv[i]);
-		}
-	printf("%i\n", res);
+	while (n)
+	{
+		if (n >= 25)
+			n -= 25;
+		else if (n >= 10)
+			n -= 10;
+		else if (n >= 5)
+			n -= 5;
+		else if (n >= 2)
+			n -= 2;
+		else if (n >= 1)
+			n -= 1;
+		coins++;
+	}
+	return (coins);
+}
+
+/**
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if no errors
+ */
+int main(int argc, char **argv)
+{
+	int number;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	number = atoi(argv[1]);
+	if (number < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	printf("%i\n", returnCents(number));
 	return (0);
 }
