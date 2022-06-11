536cf1a695f0fad3728f42bdb28b1ff80d20f487
colonelescobar <tiagati@yahoo.com>
2022-06-11 19:59:08 +0100

    0-whatsmyname

---
+++ .vscode/0-whatsmyname.c
@@ -0,0 +1,18 @@
+(#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This program prints its name, followed by a new line.
+ * @argc: arguement count.
+ * @argv: arguement content/strings.
+ * Return: int.
+ */
+
+
+int main(int argc, char *argv[])
+{
+	(__UNUSED_PARAM) argc;
+	printf("%s\n", argv[]);
+	return (0);
+
+}
