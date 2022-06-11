2c5fe1827cd8348e4fe3dbbdabf8d77ed383fc5a
colonelescobar <tiagati@yahoo.com>
2022-06-11 20:46:05 +0100

    mulc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,11 +8,16 @@
  * Return: int.
  */
 
-
 int main(int argc, char *argv[])
 {
 	(void) argc;
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
 
-	return (0);
+	printf("Error\n");
+	return (1);
+
 }
