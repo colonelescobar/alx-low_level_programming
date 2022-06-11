1a5872c9d89ecf5d99c2af24bb9c0decf085d49e
colonelescobar <tiagati@yahoo.com>
2022-06-11 20:15:00 +0100

    1-args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 
 /**
- *  
- * 
+ * main - This program prints the number of arguements passed to it.
+ * @argc: arguement count.
+ * @argv: arguement content/strings.
+ * Return: int.
  */
+
+
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	printf("%d\n", argc);
+	return (0);
+
+}
