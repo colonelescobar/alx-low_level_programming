c9d2f36ae169bc035129643a1bcbd7091ebf9bbd
colonelescobar <tiagati@yahoo.com>
2022-06-11 20:24:53 +0100

    2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 
 /**
- *  
- * 
+ * main - This program prints all arguements it recieves.
+ * @argc: arguement count.
+ * @argv: arguement content/strings.
+ * Return: int.
  */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+
+}
