af045f33ed0db20c1c698bf98c6c12e323cdaa59
colonelescobar <tiagati@yahoo.com>
2022-06-13 00:20:29 +0100

    creation of main files and task 0

--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,17 +1,8 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+#define SIZE 1024
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int s;
 
-    s = 98 + SIZE;
-    printf("%d\n", s);
-    return (0);
-}
+
+
+#endif
